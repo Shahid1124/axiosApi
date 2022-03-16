a769ba15c081c5a15e4b970b59a8f431a6f29a4f
Shahid <shahid.anis009@gmail.com>
2022-03-16 17:05:02 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+        <News />
+      </header>
+    </div>
+  );
+}
+
+export default App;
